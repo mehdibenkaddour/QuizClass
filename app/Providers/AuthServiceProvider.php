74b75cea5a153261a8654c3f76c71147449634b0
HAMDAOUI Mohammed Yassin <medyassinham@gmail.com>
2020-06-24 21:34:10 +0100

    Update token expire to 1 year on passport

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,7 +26,7 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
-        Passport::tokensExpireIn(now()->addHours(1));
+        Passport::tokensExpireIn(now()->addYears(1));
 
         Passport::refreshTokensExpireIn(now()->addDays(1));
 

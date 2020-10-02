ade1f8e7328ca173dffba361b9827013b4f2e744
HAMDAOUI Mohammed Yassin <medyassinham@gmail.com>
2020-10-02 03:56:35 +0100

    dep

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Se connecter à Iterview') }}</div>
+                <div class="card-header">{{ __('Se connecter à QuizClass') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __("S'inscrire à Iterview") }}</div>
+                <div class="card-header">{{ __("S'inscrire à QuizClass") }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
